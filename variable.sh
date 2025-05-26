a5078c4068c59686d77e371865fb6f8b769aa84f
radha <basireddyradha6@gmail.com>
2025-05-26 17:17:31 +0530

    adding

--- variable.sh
+++ variable.sh
@@ -3,6 +3,6 @@
 PERSON1=$PERSON1
 PERSION2=$PERSION2
 
-echo "$PERSON1= Hhi $PERSON2"
+echo "$PERSON1 :: Hhi $PERSON2"
 
-echo "$PERSON2= Hello $PERSON1"
+echo "$PERSON2 ::Hello ,how are you $PERSON1"
